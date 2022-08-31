8d4409d723aabc24564b358e6773ef61f7f9891a
ullltimate <ullltimate.zp@gmail.com>
2022-08-31 17:21:24 +0300

    feat: add animate

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -2,22 +2,22 @@ var button = document.querySelector('.collapsible__button');
 var content = document.querySelector('.collapsible__content');
 var hiddenButton = document.querySelector('.collapsible__action--hidden');
 var visibleButton = document.querySelector('.collapsible__action--visible');
-function buttonHiddenText(){
+function showContent(){
     hiddenButton.style.display = 'none';
     visibleButton.style.display = 'block';
-    content.style.display = 'block';
+    content.animate([{opacity: 1}], {duration:500, fill:'forwards'});
 }
-function buttonVisibleText(){
-    content.style.display = 'none';
-    visibleButton.style.display = 'none';
-    hiddenButton.style.display = 'block';
+function hideContent(){
+	content.animate([{opacity: 0}], {duration:500, fill:'forwards'});
+	visibleButton.style.display = 'none';
+	hiddenButton.style.display = 'block';
 }
-buttonHiddenText();
+showContent();
 button.addEventListener('click', function(){
     if (hiddenButton.style.display === 'none'){
-        buttonVisibleText();
+    	hideContent();
     } else {
-        buttonHiddenText();
+    	showContent();
     }  
 })
 
