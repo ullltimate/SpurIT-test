5c3163a9415926694c517eba9b515ffc0635c52f
ullltimate <ullltimate.zp@gmail.com>
2022-08-31 14:50:33 +0300

    feat: removed code duplication

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -2,19 +2,23 @@ var button = document.querySelector('.collapsible__button');
 var content = document.querySelector('.collapsible__content');
 var hiddenButton = document.querySelector('.collapsible__action--hidden');
 var visibleButton = document.querySelector('.collapsible__action--visible');
-hiddenButton.style.display = 'none';
-visibleButton.style.display = 'block';
-content.style.display = 'block';
+function buttonHiddenText(){
+    hiddenButton.style.display = 'none';
+    visibleButton.style.display = 'block';
+    content.style.display = 'block';
+}
+function buttonVisibleText(){
+    content.style.display = 'none';
+    visibleButton.style.display = 'none';
+    hiddenButton.style.display = 'block';
+}
+buttonHiddenText();
 button.addEventListener('click', function(){
     if (hiddenButton.style.display === 'none'){
-        content.style.display = 'none';
-        visibleButton.style.display = 'none';
-        hiddenButton.style.display = 'block';
+        buttonVisibleText();
     } else {
-        hiddenButton.style.display = 'none';
-        visibleButton.style.display = 'block';
-  		content.style.display = 'block';
-   }  
+        buttonHiddenText();
+    }  
 })
 
 
